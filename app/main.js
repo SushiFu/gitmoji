65d2cba744f653ff09350f5c1bc7f73344dcd2ac
Unknown <bendixon50@gmail.com>
2017-02-17 12:04:57 +0000

    :fire: Removed dev settings for app
    Removed opening of devtools on start and app always beinhg on top

--- app/main.js
+++ app/main.js
@@ -8,12 +8,8 @@ const menubar = Menubar({
     width: 400,
     height: 576,
     index: 'file://' + path.join(__dirname, 'app.html'),
-    alwaysOnTop: true,
     icon: __dirname + '/menubar_icon@2x.png',
     resizable: false,
     title: "gitmoji"
 });
 menubar.on('ready', () => {});
-menubar.on('after-create-window', () => {
-    menubar.window.openDevTools()
-})
