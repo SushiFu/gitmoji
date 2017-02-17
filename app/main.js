123b3c058e4a508a961083d8005c3649631ef4ca
Unknown <bendixon50@gmail.com>
2017-02-17 12:04:18 +0000

    :bento: Updated menubar icon

--- app/main.js
+++ app/main.js
@@ -8,8 +8,8 @@ const menubar = Menubar({
     width: 400,
     height: 576,
     index: 'file://' + path.join(__dirname, 'app.html'),
-    icon: 'file://' + path.join(__dirname, 'menubar_icon.png'),
     alwaysOnTop: true,
+    icon: __dirname + '/menubar_icon@2x.png',
     resizable: false,
     title: "gitmoji"
 });
