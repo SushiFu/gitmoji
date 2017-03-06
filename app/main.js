a5a31157506a452b00cac5a376d0808fc4c8932c
Unknown <bendixon50@gmail.com>
2017-03-06 13:31:38 +0000

    :sparkles: Hello Pug!

--- app/main.js
+++ app/main.js
@@ -2,12 +2,15 @@
 const path = require('path')
 const menubar = require('menubar')
 
+// PugJS
+require('electron-pug')({pretty: true})
+
 // Menubar Init
 const Menubar = menubar({
     width: 376,
     height: 576,
     resizable: false,
     title: 'gitmoji',
-    index: 'file://' + path.join(__dirname, 'app.html')
+    index: 'file://' + path.join(__dirname, 'index.pug')
 })
 Menubar.on('ready', () => {})
