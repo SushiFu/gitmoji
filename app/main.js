a431fd112faf8cdbd9481f5d82cbc2a02024bdd5
SushiFu <SushiFu@gmx.fr>
2017-04-25 15:03:15 +1000

    :sparkles: Close window after click & paste on clipboard (#2)

--- app/main.js
+++ app/main.js
@@ -8,11 +8,14 @@ const menubar = require('menubar')
 require('electron-pug')({pretty: true})
 
 // Menubar Constructor
-const Menubar = menubar({
+const mb = menubar({
     width: 376,
     height: 576,
     resizable: false,
     title: 'gitmoji',
     index: 'file://' + path.join(__dirname, 'index.pug')
 })
-Menubar.on('ready', () => {})
+
+mb.on('ready', () => { })
+
+global.menubar = mb
